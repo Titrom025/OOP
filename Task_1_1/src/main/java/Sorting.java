3894312393bf1767b2392d85206ec9de3272dba8
Titrom025 <titrom25@mail.ru>
2020-09-17 19:30:21 +0700

    Updated Task_1_!

--- Task_1_1/src/main/java/Sorting.java
+++ Task_1_1/src/main/java/Sorting.java
@@ -1,9 +1,4 @@
-import java.util.Arrays;
-
 public class Sorting {
-
-    private static int[] heap = new int[4];
-    private static int k = 0;
 
     /**
      * This function swap two elements with indexes firstIndex and secondIndex in array
@@ -19,88 +14,40 @@ public class Sorting {
     }
 
     /**
-     * Sifts the element up with index X in the heap
-     *
-     * @param x index of element to sift up
-     */
-    private static void siftup(int x) {
-        if (x == 0) {
-            return;
-        }
-
-        int parent = (x - 1) / 2;
-
-        if (heap[x] < heap[parent]) {
-            swap(heap, x, parent);
-            siftup(parent);
-        }
-    }
-
-    /**
      * Sifts the element down with index X in the heap
      *
-     * @param x index of element to sift down
+     * @param index index of element to sift down
      */
-    private static void siftdown(int x) {
-        int a = x * 2 + 1;
-        int b = x * 2 + 2;
+    private static void siftdown(int[] array, int index, int size) {
+        int largest = index;
+        int leftSon = index * 2 + 1;
+        int rightSon = index * 2 + 2;
 
-        if (a < k && b < k) {
-            if (heap[a] < heap[b]) {
-                if (heap[a] < heap[x]) {
-                    swap(heap, x, a);
-                    siftdown(a);
-                }
-            } else {
-                if (heap[b] < heap[x]) {
-                    swap(heap, x, b);
-                    siftdown(b);
-                }
-            }
-        } else if (a < k) {
-            if (heap[a] < heap[x]) {
-                swap(heap, x, a);
-                siftdown(a);
-            }
+        if (leftSon < size && array[leftSon] > array[largest]) {
+            largest = leftSon;
         }
-    }
 
-    /**
-     * The method used for adding new element with value x in the heap
-     *
-     * @param x the value to add in the heap
-     */
-    private static void addElement(int x) {
-        if (k == heap.length) {
-            heap = Arrays.copyOf(heap, heap.length * 2 + 1);
+        if (rightSon < size && array[rightSon] > array[largest]) {
+            largest = rightSon;
         }
-        heap[k++] = x;
-        siftup(k - 1);
-    }
 
-    /**
-     * Yhe metod returns minimum value
-     * from heap and starts siftdown from zero element
-     *
-     * @return minimum value from heap
-     */
-    private static int extractMinimum() {
-        int minimum = heap[0];
-        swap(heap, 0, --k);
-        siftdown(0);
-        return minimum;
+        if (largest != index) {
+            swap(array, index, largest);
+            siftdown(array, largest, size);
+        }
     }
 
     public static void sortArray(int[] array) {
 
-        int elementCount = array.length;
+        int elementsCount = array.length;
 
-        for (int number : array) {
-            addElement(number);
+        for (int index = elementsCount / 2 - 1; index >= 0 ; index--) {
+            siftdown(array, index, elementsCount);
         }
 
-        for (int i = 0; i < elementCount; i++) {
-            array[i] = extractMinimum();
+        for (int index = elementsCount - 1; index > 0; index--) {
+            swap(array, 0, index);
+            siftdown(array, 0, index);
         }
     }
 }
--- Task_1_1/src/test/java/SortingTest.java
+++ Task_1_1/src/test/java/SortingTest.java
@@ -4,20 +4,14 @@ import org.junit.Assert;
 import java.util.Arrays;
 import java.util.Random;
 
-import static java.lang.System.out;
-
 public class SortingTest extends TestCase {
 
     public void testSortArray1() {
         int[] arr = new int[]{13,15,9,-5,8,6,0,3,53,63,7,2,1,5,7,2};
         int[] answer = arr.clone();
 
-        out.println("Input: " + Arrays.toString(arr));
-
         Arrays.sort(answer);
         Sorting.sortArray(arr);
-
-        out.println("Output: " + Arrays.toString(arr));
 
         Assert.assertArrayEquals(arr, answer);
     }
@@ -26,12 +20,8 @@ public class SortingTest extends TestCase {
         int[] arr = new int[]{-1123,-13,23,-131,-213,-4343,-3422,13,1231,42};
         int[] answer = arr.clone();
 
-        out.println("Input: " + Arrays.toString(arr));
-
         Arrays.sort(answer);
         Sorting.sortArray(arr);
-
-        out.println("Output: " + Arrays.toString(arr));
 
         Assert.assertArrayEquals(arr, answer);
     }
@@ -40,28 +30,26 @@ public class SortingTest extends TestCase {
 
         Random random = new Random();
 
-        for (int i = 0; i < 500; i++) {
+        int numberOfTest = 1000;
+        int maxArrayLength = 2000;
+        int maxElementValue = 1000000;
 
-            int arrayLength = random.nextInt(490) + 10;
+        for (int i = 0; i < numberOfTest; i++) {
+
+            int arrayLength = random.nextInt(maxArrayLength);
 
             int[] arrayToSort = new int[arrayLength];
 
             for (int j = 0; j < arrayLength; j++) {
-                arrayToSort[j] = random.nextInt(200000) - 100000;
+                arrayToSort[j] = random.nextInt(maxElementValue) - maxElementValue / 2;
             }
 
             int[] arraySorted = arrayToSort.clone();
 
-            out.println("Input: " + Arrays.toString(arrayToSort));
-
             Arrays.sort(arraySorted);
             Sorting.sortArray(arrayToSort);
 
-            out.println("Output: " + Arrays.toString(arraySorted));
-
             Assert.assertArrayEquals(arraySorted, arrayToSort);
-
-            System.gc();
         }
     }
 }
