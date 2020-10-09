747c67d65292a722c39625b9a10d555ceb84e7ff
Titrom025 <titrom25@mail.ru>
2020-10-09 13:10:58 +0700

    Added try-with-resources

--- Task_1_2/src/main/java/Task_1_2/SubstringsFinder/SubstringsFinder.java
+++ Task_1_2/src/main/java/Task_1_2/SubstringsFinder/SubstringsFinder.java
@@ -1,4 +1,5 @@
 package Task_1_2.SubstringsFinder;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -12,73 +13,64 @@ public class SubstringsFinder {
      * The function finds all occurrences of substring "substring"
      * in the file with name "filenameFromResources"
      *
-     * @param filenameFromResources  name of input file
-     * @param substring content of substring
+     * @param file input file
+     * @param substring             content of substring
      * @return array of position of occurrences
      * @throws IOException
      */
-    public static long[] findSubstringInFile(String filenameFromResources, String file, String substring) throws IOException {
+    public static long[] findSubstringInFile(File file, String substring) throws IOException {
 
-        long[] answersArray = new long[DEFAULT_ANSWER_ARRAY_SIZE];
-        int answerCount = 0;
-        int substringLength = substring.length();
-        int countOfcharactersRead;
-        int stringNumber = 0;
+        try (BufferedReader input = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
 
-        File inputFile;
+            long[] answersArray = new long[DEFAULT_ANSWER_ARRAY_SIZE];
+            int answerCount = 0;
+            int substringLength = substring.length();
+            int countOfcharactersRead;
+            int stringNumber = 0;
 
-        if (file.equals("")) {
-            ClassLoader classLoader = SubstringsFinder.class.getClassLoader();
-            inputFile = new File(classLoader.getResource(filenameFromResources).getFile());
-        } else {
-            inputFile = new File(file);
-        }
+            char[] chars = new char[SECTION_LENGTH + substringLength];
 
-        FileReader filereader = new FileReader(inputFile, StandardCharsets.UTF_8);
-        BufferedReader input = new BufferedReader(filereader);
-
-        char[] chars = new char[SECTION_LENGTH + substringLength];
-
-        for (int i = 0; i < substringLength; i++) {
-            chars[i] = substring.charAt(i);
-        }
-
-        input.read(chars, 0, substringLength);
-        while ((countOfcharactersRead = input.read(chars, substringLength, SECTION_LENGTH)) != -1) {
-
-            String string = new String(chars).substring(0, countOfcharactersRead + substringLength);
-
-            if (countOfcharactersRead != SECTION_LENGTH) {
-                string = string.substring(0, countOfcharactersRead + substringLength);
+            for (int i = 0; i < substringLength; i++) {
+                chars[i] = substring.charAt(i);
             }
 
-            int indexOfSubstring = -1;
-            while ((indexOfSubstring = string.indexOf(substring, indexOfSubstring + 1)) != -1 ) {
-                long answer =  indexOfSubstring + stringNumber * SECTION_LENGTH;
-                if (answerCount == answersArray.length) {
-                    answersArray = Arrays.copyOf(answersArray, answersArray.length * 2 + 1);
+            input.read(chars, 0, substringLength);
+            while ((countOfcharactersRead = input.read(chars, substringLength, SECTION_LENGTH)) != -1) {
+
+                String string = new String(chars).substring(0, countOfcharactersRead + substringLength);
+
+                if (countOfcharactersRead != SECTION_LENGTH) {
+                    string = string.substring(0, countOfcharactersRead + substringLength);
                 }
-                answersArray[answerCount++] = answer;
 
+                int indexOfSubstring = -1;
+                while ((indexOfSubstring = string.indexOf(substring, indexOfSubstring + 1)) != -1) {
+                    long answer = indexOfSubstring + stringNumber * SECTION_LENGTH;
+                    if (answerCount == answersArray.length) {
+                        answersArray = Arrays.copyOf(answersArray, answersArray.length * 2 + 1);
+                    }
+                    answersArray[answerCount++] = answer;
+
+                }
+
+                if (countOfcharactersRead == SECTION_LENGTH) {
+                    String newString = string.substring(SECTION_LENGTH, SECTION_LENGTH + substringLength);
+                    string = string.substring(0, SECTION_LENGTH);
+                    newString = newString.concat(string);
+                    chars = newString.toCharArray();
+                }
+
+                stringNumber++;
             }
 
-            if (countOfcharactersRead == SECTION_LENGTH) {
-                String newString = string.substring(SECTION_LENGTH, SECTION_LENGTH + substringLength);
-                string = string.substring(0, SECTION_LENGTH);
-                newString = newString.concat(string);
-                chars = newString.toCharArray();
+            if (stringNumber == 0) {
+                String stringFromChars = new String(chars).trim();
+                if (substring.equals(stringFromChars)) {
+                    answersArray[answerCount++] = 0;
+                }
             }
 
-            stringNumber++;
+            return Arrays.copyOf(answersArray, answerCount);
         }
-
-        if (stringNumber == 0) {
-            String stringFromChars = new String(chars).trim();
-            if (substring.equals(stringFromChars)) {
-                answersArray[answerCount++] = 0;
-            }
-        }
-
-        return Arrays.copyOf(answersArray, answerCount);
     }
 }
