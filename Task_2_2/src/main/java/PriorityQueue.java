d9dc856cba7bf4e5912091031bde6ce95bec5a1b
Titrom025 <titrom25@mail.ru>
2020-10-16 00:07:24 +0700

    Added Splitterator

--- Task_2_2/src/main/java/PriorityQueue.java
+++ Task_2_2/src/main/java/PriorityQueue.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 public class PriorityQueue<K extends Comparable<K>, V> implements Iterable<Pair<K, V>>{
 
@@ -76,6 +77,57 @@ public class PriorityQueue<K extends Comparable<K>, V> implements Iterable<Pair<
     @Override
     public Iterator<Pair<K, V>> iterator() {
         return new PriorityQueueIterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Pair<K, V>> action) {
+
+    }
+
+    @Override
+    public Spliterator<Pair<K, V>> spliterator() {
+        return new PriorityQueueSpliterator<Pair<K, V>>(queue,0, elementsCount);
+    }
+
+    static class PriorityQueueSpliterator<T> implements Spliterator<T> {
+        private final Object[] array;
+        private int maxPos; // current index, advanced on split or traversal
+        private int currentPos = 0;
+
+        PriorityQueueSpliterator(Object[] array, int currentPos, int maxPos) {
+            this.array = array;
+            this.currentPos = currentPos;
+            this.maxPos = maxPos;
+        }
+        @Override
+        public boolean tryAdvance(Consumer<? super T> action) {
+            if (currentPos < maxPos) {
+                action.accept((T) array[currentPos++]);
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Spliterator<T> trySplit() {
+            int middle = (maxPos + currentPos) / 2;
+            if (currentPos < middle && middle < maxPos) {
+                int newSpliteratorBottomLimit = currentPos;
+                currentPos = middle;
+                return new PriorityQueueSpliterator<T>(array, newSpliteratorBottomLimit, middle);
+            }
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return maxPos - currentPos;
+        }
+
+        @Override
+        public int characteristics() {
+            return SIZED | SUBSIZED | NONNULL;
+        }
     }
 
     class PriorityQueueIterator implements Iterator<Pair<K, V>> {
--- Task_2_2/src/test/java/PriorityQueueTest.java
+++ Task_2_2/src/test/java/PriorityQueueTest.java
@@ -1,24 +1,53 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
 public class PriorityQueueTest {
 
     @Test
     public void testPriorityQueue1() {
+        PriorityQueue<Integer, String> queue = new PriorityQueue<>();
+
+        queue.insert(200, "собака");
+        queue.insert(10, "человек");
+
+        System.out.println(queue.extract_max());
+
+        queue.insert(5, "пингвин");
+        queue.insert(500, "попугай");
+
+        System.out.println(queue.extract_max());
+    }
+
+    @Test
+    public void testPriorityQueue2() {
 
         PriorityQueue<Integer, String> queue = new PriorityQueue<>();
 
         queue.insert(200, "собака");
-        queue.insert(10, "111");
-        System.out.println(queue.extract_max());
-        queue.insert(500, "попугай");
+        queue.insert(10, "человек");
         queue.insert(5, "пингвин");
+        queue.insert(500, "попугай");
 
         for (Pair<Integer, String> pair : queue) {
             System.out.printf("Key: %d, value: %s\n", pair.key, pair.value);
         }
+    }
 
-        System.out.println(queue.extract_max());
+    @Test
+    public void testPriorityQueue3() {
 
+        PriorityQueue<Integer, String> queue = new PriorityQueue<>();
+
+        queue.insert(200, "собака");
+        queue.insert(10, "человек");
+        queue.insert(5, "пингвин");
+        queue.insert(500, "попугай");
+
+        Object[] values = StreamSupport.stream(queue.spliterator(), false).map((a) -> a.value).toArray();
+
+        System.out.println(Arrays.toString(values));
     }
 
 }
